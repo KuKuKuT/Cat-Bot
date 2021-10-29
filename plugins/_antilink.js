1179431879f19aeca3156add425b9d17caa76b5c
KuKuKuT <92572980+KuKuKuT@users.noreply.github.com>
2021-10-29 13:01:29 +0530

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,7 +9,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
     let thisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
     if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-    await this.sendButon(m.chat, `*😼 Cat has Detected a link here!*${isBotAdmin ? '' : '\n\nI am not an admin so I can't kick 😿'}\n\nType *.off antilink* to turn off this feature${opts['restrict'] ? '' : '\nType *#on restrict* so you can kick 😺'}`, '© TOXIC DEVIL', 'Turn off Antilink 😿', ',0 antilink', m)
+    await this.sendButon(m.chat, `*😼 Cat has Detected a link here!*${isBotAdmin ? '' : '\n\nI am not an admin so I can't kick 😿'}\n\nType *.off antilink* to turn off this feature${opts['restrict'] ? '' : '\nType *#on restrict* so you can kick 😺'}`, '© TOXIC DEVIL', 'Turn off Antilink 😿', ',0 antilink')
     if (global.opts['restrict']) {
       if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
     }

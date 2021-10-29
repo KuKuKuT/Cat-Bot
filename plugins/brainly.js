04e6c08d3bed7b4e341357ac47021dc5b33d6855
KuKuKuT <92572980+KuKuKuT@users.noreply.github.com>
2021-10-29 12:56:40 +0530

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -2,7 +2,7 @@ const Brainly = require('brainly-scraper-v2')
 const brain = new Brainly("en")
 let handler = async function (m, { text, usedPrefix, command }) {
   if (!text) throw `*😿 You must enter a query!*\n😺 Example:\n${usedPrefix + command} What is nodejs?`
-  brain.search("en", text).then(async res => {
+  brain.search("us", text).then(async res => {
     let br = JSON.stringify(res)
     let json = JSON.parse(br)
     let answer = json.map((v, i) => `*Question To: ${i + 1}*\n${v.question.content}\n${v.answers.map((v, i) => `*Answer To: ${i + 1}*\n${v.content.replace(/<\/?p>|<\/?strong>|<\/?u>|<\/?h[1-3]>|<\/?span>/g, '').replace(/<br ?(\/|\\)?>/g, '\n')}`).join('\n')}`).join('\n\n•_-_-_-_-_-_•\n\n')
